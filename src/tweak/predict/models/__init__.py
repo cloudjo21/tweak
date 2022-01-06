25a4397c40b89128561bbffe24bf21fc3253fd33
yhcho-asc <yhcho-asc@ascentkorea.co.jp>
2022-01-06 13:39:53 +0900

    [Python] feat: add model output for prediction result #1

--- src/tweak/predict/models/__init__.py
+++ src/tweak/predict/models/__init__.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import List, Optional
 
 from tweak.orjson_utils import *
@@ -15,8 +15,22 @@ class ModelConfig(BaseModel):
         json_dumps = orjson_dumps
 
 
-class ModelOutput(BaseModel):
-    logits: List[int]
+class ModelOutput(ABC):
+    pass
+
+
+import numpy as np
+import torch
+
+# TODO for prediction result by prediction build command
+class ModelOutputForTokenClassification(BaseModel, ModelOutput):
+    logits: list
+    @validator('logits', pre=True)
+    def parse_logits(v):
+        return v.tolist()
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
 class PredictableModel(ABC):

144099032b2bae0c22970ff60e147576c09469b1
yhcho-asc <yhcho-asc@ascentkorea.co.jp>
2022-02-15 23:59:58 +0900

    [Python] fix: fix to run download #12

--- src/tweak/predict/resource_materialize.py
+++ src/tweak/predict/resource_materialize.py
@@ -8,35 +8,39 @@ from tweak.predict.config import TokenizerConfig
 
 
 class ResourceMaterializer:
-
     @classmethod
-    def apply_for_tokenizer(cls, config: TokenizerConfig, service_config: ServiceLevelConfig):
+    def apply_for_tokenizer(
+        cls, config: TokenizerConfig, service_config: ServiceLevelConfig
+    ):
         webhdfs = HttpBasedWebHdfsFileHandler(service_config)
-        count = 0
-        print(count)
-        count += 1
 
-        cls._apply_if_service_from_hdfs(f"{config.model_path}/config.json", webhdfs, service_config)
+        cls._apply_if_service_from_hdfs(
+            f"{config.model_path}/config.json", webhdfs, service_config
+        )
         config.model_path = f"{service_config.local_prefix}/{config.model_path}"
-        print(count)
-        count += 1
 
         if config.path:
             cls._apply_if_service_from_hdfs(config.path, webhdfs, service_config)
             config.path = f"{service_config.local_prefix}/{config.path}"
-        print(count)
-        count += 1
 
     @classmethod
-    def apply_for_hf_model(cls, config: Union[ModelConfig, PreTrainedModelConfig], service_config: ServiceLevelConfig):
+    def apply_for_hf_model(
+        cls,
+        config: Union[ModelConfig, PreTrainedModelConfig],
+        service_config: ServiceLevelConfig,
+    ):
         webhdfs = HttpBasedWebHdfsFileHandler(service_config)
 
         cls._apply_if_service_from_hdfs(config.model_path, webhdfs, service_config)
         config.model_path = f"{service_config.local_prefix}/{config.model_path}"
 
-
     @classmethod
-    def _apply_if_service_from_hdfs(cls, path: str, file_handler: HttpBasedWebHdfsFileHandler, service_config: ServiceLevelConfig):
-        print(path)
-        if service_config.filesystem.upper() == 'HDFS':
-            file_handler.download(path)
+    def _apply_if_service_from_hdfs(
+        cls,
+        path: str,
+        file_handler: HttpBasedWebHdfsFileHandler,
+        service_config: ServiceLevelConfig,
+    ):
+        if service_config.filesystem.upper() == "HDFS":
+            # file_handler.download(path)
+            file_handler.download(path, read_mode='rb', write_mode='wb')

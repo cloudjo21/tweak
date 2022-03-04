29909dc62af874a8cf1625274ae12edea812e5f1
yhcho-asc <yhcho-asc@ascentkorea.co.jp>
2022-03-04 09:58:05 +0900

    [Python] test: test plm predictions

--- src/tweak/test/tweak/predict/test_tokenizers.py
+++ src/tweak/test/tweak/predict/test_tokenizers.py
@@ -9,6 +9,7 @@ from tunip.path_utils import services as path_services
 from tunip.service_config import get_service_config
 
 from tweak.predict.config import TokenizerConfig
+from tweak.predict.predictor import PredictorConfig
 from tweak.predict.resource_materialize import ResourceMaterializer
 
 
@@ -31,3 +32,23 @@ class TokenizersTest(unittest.TestCase):
         ResourceMaterializer.apply_for_tokenizer(config, service_config)
         vocab_path = f"{NAUTS_LOCAL_ROOT}/user/nauts/mart/tokenizers/monologg%2Fkoelectra-small-v3-discriminator/vocab.txt"
         assert pathlib.Path(vocab_path).exists()
+
+    def test_load_predictor_config_and_access_config(self):
+
+        predictor_config = {
+            "predict_tokenizer_type": "auto",
+            "predict_model_type": "torchscript",
+            "model_config": {
+                "model_path": "/user/nauts/mart/plm/models/monologg%2Fkoelectra-small-v3-discriminator/torchscript",
+                "model_name": "monologg/koelectra-small-v3-discriminator"
+            },
+            "tokenizer_config": {
+                "model_path": "/user/nauts/mart/plm/models/monologg%2Fkoelectra-small-v3-discriminator",
+                "path": "/user/nauts/mart/tokenizers/monologg%2Fkoelectra-small-v3-discriminator",
+                "max_length": 128
+            }
+        }
+
+        pred_config = PredictorConfig.parse_obj(predictor_config)
+        assert pred_config.tokenizer_config
+        assert pred_config.tokenizer_config.max_length == 128

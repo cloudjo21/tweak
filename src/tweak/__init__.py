0cdf9da8aa08d650a296927dfc46e2ab06a5d1f9
yhcho-asc <yhcho-asc@ascentkorea.co.jp>
2022-03-09 17:55:04 +0900

    [Python] feat: add logger #19

--- src/tweak/__init__.py
+++ src/tweak/__init__.py
@@ -2,3 +2,7 @@ from .predict import *
 from .task import *
 from .utils import *
 from .orjson_utils import *
+
+from tunip.logger import init_logging_handler
+
+LOGGER = init_logging_handler(name="tweak")

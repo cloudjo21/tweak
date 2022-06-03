fcb3ec89fb8d838822bf36d72ab258b4c03d7ba7
ed-codit <ed-codit@thecodit.com>
2022-06-03 15:59:59 +0900

    chore: update v0.0.2

--- src/tweak/test/tweak/train/nn_clustering/test_jaccard_dist.py
+++ src/tweak/test/tweak/train/nn_clustering/test_jaccard_dist.py
@@ -27,50 +27,51 @@ class JaccardDistTest(unittest.TestCase):
         self.white_ptags = ['V', 'N', 'J', 'M', 'SL', 'SH', 'SN']
 
         self.distance_calc = BigramJaccardDistanceCalc()
-        self.max_row = 100
-
+        self.max_rows = [100, 150, 200]
 
     def test_search_and_tokenize(self):
-        search_res_size = self.max_row
+        for max_row in self.max_rows:
+            search_res_size = max_row
 
-        query_example = "content(넷플릭스) AND content(규제)"
-        query = {
-            "query": {
-                "query_string": {
-                "query": query_example 
-                }
-            },
-            "size": search_res_size
-        }
-        es_results = self.es.search(query, index=self.index_name)
+            query_example = "content(넷플릭스) AND content(규제)"
+            query = {
+                "query": {
+                    "query_string": {
+                    "query": query_example 
+                    }
+                },
+                "size": search_res_size
+            }
+            es_results = self.es.search(query, index=self.index_name)
 
-        titles = []
-        es_doc_ids = []
-        for result in es_results['hits']['hits']:
-            titles.append(self.re_norm_nums.sub('N', result["_source"]["title"]))
-            es_doc_ids.append(result['_id'])
+            titles = []
+            es_doc_ids = []
+            for result in es_results['hits']['hits']:
+                titles.append(self.re_norm_nums.sub('N', result["_source"]["title"]))
+                es_doc_ids.append(result['_id'])
 
 
-        bigrams = self.nugget.bigrams(
-            titles, white_tags=self.white_ptags, result_format=NuggetFilterResultFormat.NUGGET_B_E_LEX
-        )
-        assert len(bigrams) > 0 and len(bigrams[0]) > 0
+            bigrams = self.nugget.bigrams(
+                titles, white_tags=self.white_ptags, result_format=NuggetFilterResultFormat.NUGGET_B_E_LEX
+            )
+            assert len(bigrams) > 0 and len(bigrams[0]) > 0
 
-        corpus = [' '.join(map('_'.join, bigram_tokens)) for bigram_tokens in bigrams]
-        vectorizer = CountVectorizer()
+            corpus = [' '.join(map('_'.join, bigram_tokens)) for bigram_tokens in bigrams]
+            vectorizer = CountVectorizer()
 
-        # scipy.sparse._csr.csr_matrix
-        xx = vectorizer.fit_transform(corpus)
-         # numpy.ndarray
-        term_vector = xx.toarray()
+            # scipy.sparse._csr.csr_matrix
+            xx = vectorizer.fit_transform(corpus)
+            # numpy.ndarray
+            import numpy as np
+            term_vector = xx.toarray().astype(np.int8)
 
-        dist_calc_req = JaccardDistanceCalcRequest(term_vector= term_vector, num_rows=self.max_row) # TODO set config from deploy
-        start = timeit.default_timer()
-        dist_calc_res: JaccardDistanceCalcResponse = self.distance_calc(dist_calc_req)
-        duration_in_ms = timeit.default_timer() - start
-        print(f"dist calc took {duration_in_ms} ms.")
+            dist_calc_req = JaccardDistanceCalcRequest(term_vector= term_vector, num_rows=max_row) # TODO set config from deploy
+            start = timeit.default_timer()
+            dist_calc_res: JaccardDistanceCalcResponse = self.distance_calc(dist_calc_req)
+            duration_in_ms = timeit.default_timer() - start
+            print(f"dist calc took {duration_in_ms} ms.")
 
-        # < 50ms for 100 es result docs
-        # < 100ms for 150 es result docs
-        # < 200ms for 200 es result docs
-        assert duration_in_ms < 0.050
+            # < 50ms for 100 es result docs
+            # < 100ms for 150 es result docs
+            # < 200ms for 200 es result docs
+            assert duration_in_ms < 0.150

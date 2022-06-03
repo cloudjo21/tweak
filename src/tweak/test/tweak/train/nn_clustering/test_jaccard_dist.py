174ac04aa0eaedb2dbcba7455b1cbe256bd62eea
ed-codit <ed-codit@thecodit.com>
2022-06-03 13:41:29 +0900

    test: test to measure proc. time for jaccard distance (NTS-46)

---
+++ src/tweak/test/tweak/train/nn_clustering/test_jaccard_dist.py
@@ -0,0 +1,76 @@
+import unittest
+import re
+import timeit
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+from tunip.es_utils import init_elastic_client
+from tunip.nugget_api import Nugget, NuggetFilterResultFormat
+from tunip.service_config import get_service_config
+from tweak.clustering.distance.bigram_jaccard_dist import (
+    BigramJaccardDistanceCalc,
+    JaccardDistanceCalcRequest,
+    JaccardDistanceCalcResponse
+)
+
+
+class JaccardDistTest(unittest.TestCase):
+
+    def setUp(self):
+        self.index_name = 'news-2022-v1'
+
+        self.re_norm_nums = re.compile('\d+')
+        self.es = init_elastic_client(get_service_config())
+        self.es.indices.exists(self.index_name)
+
+        self.nugget = Nugget()
+        self.white_ptags = ['V', 'N', 'J', 'M', 'SL', 'SH', 'SN']
+
+        self.distance_calc = BigramJaccardDistanceCalc()
+        self.max_row = 100
+
+
+    def test_search_and_tokenize(self):
+        search_res_size = self.max_row
+
+        query_example = "content(넷플릭스) AND content(규제)"
+        query = {
+            "query": {
+                "query_string": {
+                "query": query_example 
+                }
+            },
+            "size": search_res_size
+        }
+        es_results = self.es.search(query, index=self.index_name)
+
+        titles = []
+        es_doc_ids = []
+        for result in es_results['hits']['hits']:
+            titles.append(self.re_norm_nums.sub('N', result["_source"]["title"]))
+            es_doc_ids.append(result['_id'])
+
+
+        bigrams = self.nugget.bigrams(
+            titles, white_tags=self.white_ptags, result_format=NuggetFilterResultFormat.NUGGET_B_E_LEX
+        )
+        assert len(bigrams) > 0 and len(bigrams[0]) > 0
+
+        corpus = [' '.join(map('_'.join, bigram_tokens)) for bigram_tokens in bigrams]
+        vectorizer = CountVectorizer()
+
+        # scipy.sparse._csr.csr_matrix
+        xx = vectorizer.fit_transform(corpus)
+         # numpy.ndarray
+        term_vector = xx.toarray()
+
+        dist_calc_req = JaccardDistanceCalcRequest(term_vector= term_vector, num_rows=self.max_row) # TODO set config from deploy
+        start = timeit.default_timer()
+        dist_calc_res: JaccardDistanceCalcResponse = self.distance_calc(dist_calc_req)
+        duration_in_ms = timeit.default_timer() - start
+        print(f"dist calc took {duration_in_ms} ms.")
+
+        # < 50ms for 100 es result docs
+        # < 100ms for 150 es result docs
+        # < 200ms for 200 es result docs
+        assert duration_in_ms < 0.050

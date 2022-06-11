3883d09b110b5ddedd8138e051a92ca3983ad347
ed-codit <ed-codit@thecodit.com>
2022-06-11 17:29:46 +0900

    test: test to debug linkage connections for clustering (NTS-58)

---
+++ src/tweak/test/tweak/train/nn_clustering/test_notebook_hac.py
@@ -0,0 +1,538 @@
+es_result = {
+    "es_result": {
+        "hits": {
+            "hits": [
+                {
+                    "_id": "TheFact-national-1944422",
+                    "_source": {
+                        "title": "충남도립대, 충남 명품수산물 요리 경연대회"
+                    }
+                },
+                {
+                    "_id": "fnnews-livingculture-202206101059176735",
+                    "_source": {
+                        "title": "제주 드림타워 “일식당 유메야마서 새 메뉴 출시“"
+                    }
+                },
+                {
+                    "_id": "joongang-25078039",
+                    "_source": {
+                        "title": "‘오겜’ 음악감독, 세계적 레이블 데카와 손잡았다"
+                    }
+                },
+                {
+                    "_id": "mk-202206508939",
+                    "_source": {
+                        "title": "[MK 6월 3주 바다낚시] 귀어에 대한 꿈을 꾸고 있다면"
+                    }
+                },
+                {
+                    "_id": "news1-4707998",
+                    "_source": {
+                        "title": "롯데관광개발, 그랜드 하얏트 제주 일식당 ‘유메야마‘ 할인 프로모션"
+                    }
+                },
+                {
+                    "_id": "khan-opinion-202206100300035",
+                    "_source": {
+                        "title": "북극은 지금, 불덩이 바다"
+                    }
+                },
+                {
+                    "_id": "mk-202206509450",
+                    "_source": {
+                        "title": "“K팝 vs J팝, 어느쪽이 인기?“ 일본인들도 “K팝“ 인정"
+                    }
+                },
+                {
+                    "_id": "hani-society-1046416",
+                    "_source": {
+                        "title": "6월 10일 동정"
+                    }
+                },
+                {
+                    "_id": "yna-AKR20220609056700003",
+                    "_source": {
+                        "title": "GS리테일 퀵커머스 ‘요마트‘ “축산 등 신선식품이 인기“"
+                    }
+                },
+                {
+                    "_id": "edaily-01230006632360408",
+                    "_source": {
+                        "title": "[밀라노에서 온 편지]디자인 도시 밀라노에 부는 ‘K‘바람"
+                    }
+                },
+                {
+                    "_id": "hani-economy-1046478",
+                    "_source": {
+                        "title": "때 이른 무더위, 호텔 보양식으로 이겨내 볼까?"
+                    }
+                },
+                {
+                    "_id": "news1-4706872",
+                    "_source": {
+                        "title": "한류, 일본 짓눌렀다…한일 양국서 ‘K-컬쳐 위상 더 높다‘ 여론 우세"
+                    }
+                },
+                {
+                    "_id": "munhwa-2022060901031239342001",
+                    "_source": {
+                        "title": "서울시, 내달 50명 간부공무원 참여 ‘정책 연찬회’ 열어"
+                    }
+                },
+                {
+                    "_id": "itchosun-2022060901219",
+                    "_source": {
+                        "title": "‘요마트‘ 신선식품 매출 비중 38.1%"
+                    }
+                },
+                {
+                    "_id": "hani-society-1046125",
+                    "_source": {
+                        "title": "‘국민 생선’, 30년 만에 명태에서 이것으로 바뀐 이유"
+                    }
+                },
+                {
+                    "_id": "news1-4706825",
+                    "_source": {
+                        "title": "“수원시농수산물도매시장 수산물, 중금속 걱정 마세요“"
+                    }
+                },
+                {
+                    "_id": "heraldcorp-20220609000161",
+                    "_source": {
+                        "title": "GS리테일, ‘요마트’ 장바구니 보니…매출1위는 우삼겹구이"
+                    }
+                },
+                {
+                    "_id": "newsis-NISX20220609_0001902081",
+                    "_source": {
+                        "title": "수원시농수산물도매시장 수산물, 안심하고 드세요"
+                    }
+                },
+                {
+                    "_id": "yna-AKR20220609081600005",
+                    "_source": {
+                        "title": "‘오징어게임‘ 음악감독 정재일, 세계적 레이블 데카와 계약"
+                    }
+                },
+                {
+                    "_id": "news1-4706344",
+                    "_source": {
+                        "title": "요마트서 뭐가 잘 팔렸나 보니…1위는 ‘우삼겹구이‘"
+                    }
+                },
+                {
+                    "_id": "joongang-25077786",
+                    "_source": {
+                        "title": "코리아부와 샤이 K팝 팬을 넘어"
+                    }
+                },
+                {
+                    "_id": "segye-20220607521051",
+                    "_source": {
+                        "title": "울릉 농수특산물 온라인 쇼핑몰 ‘울릉몰‘ 8일 개장"
+                    }
+                },
+                {
+                    "_id": "TheFact-economy-1944248",
+                    "_source": {
+                        "title": "[엔터Biz] CGV, 5년 만에 코스피200 제외…악재일까 호재일까"
+                    }
+                },
+                {
+                    "_id": "fnnews-livingculture-202206090918512875",
+                    "_source": {
+                        "title": "GS리테일 “‘요마트‘ 베스트 상품 1위는 우삼겹“"
+                    }
+                },
+                {
+                    "_id": "lawissue-202206090852398000204ead0791_12",
+                    "_source": {
+                        "title": "GS리테일, ‘요마트’ 상위 베스트 5가지 상품 모두 축산류로 분석돼"
+                    }
+                },
+                {
+                    "_id": "mt-2022060908222037433",
+                    "_source": {
+                        "title": "‘요마트‘ 가장 많이 팔린 상품 1위는 ‘우삼겹구이‘"
+                    }
+                },
+                {
+                    "_id": "sedaily-2675YI016N",
+                    "_source": {
+                        "title": "“우럭 가격을 확 낮췄다“…물가 상승에 빛 발한 롯데마트 로컬MD"
+                    }
+                },
+                {
+                    "_id": "newsis-NISX20220608_0001899816",
+                    "_source": {
+                        "title": "[교육소식] 우송정보대, 대한산업보건협회와 산학협력 등"
+                    }
+                },
+                {
+                    "_id": "sportskhan-202206082050003540101",
+                    "_source": {
+                        "title": "유명 연예인·스포츠인 얼굴·이름 무단 사용하면 법적 제재"
+                    }
+                },
+                {
+                    "_id": "fnnews-livingculture-202206081002335637",
+                    "_source": {
+                        "title": "조선호텔, ‘중국식 냉면‘ 등 신제품 3종 출시"
+                    }
+                },
+                {
+                    "_id": "yna-AKR20220608034300063",
+                    "_source": {
+                        "title": "유명 연예인·스포츠인 얼굴·이름 무단 사용하면 법적 제재"
+                    }
+                },
+                {
+                    "_id": "heraldcorp-20220607000745",
+                    "_source": {
+                        "title": "“얼마나 급했으면 또 지겨운 좀비냐” 넷플릭스, ‘이 분’ 부활"
+                    }
+                },
+                {
+                    "_id": "sedaily-2675JX959W",
+                    "_source": {
+                        "title": "정부 물량 공세에도 여전히 金징어·金태"
+                    }
+                },
+                {
+                    "_id": "yna-AKR20220607129000053",
+                    "_source": {
+                        "title": "울릉 농수특산물 온라인 쇼핑몰 ‘울릉몰‘ 8일 개장"
+                    }
+                },
+                {
+                    "_id": "mt-2022060710270913157",
+                    "_source": {
+                        "title": "“K-좀비가 돌아온다“ 넷플릭스 ‘지금 우리 학교는‘ 시즌2 제작"
+                    }
+                },
+                {
+                    "_id": "khan-economy-202206091747001",
+                    "_source": {
+                        "title": "퀵커머스 ‘요마트‘ 장보기 매출 보니…우삼겹구이 1등"
+                    }
+                },
+                {
+                    "_id": "yna-AKR20220607017500005",
+                    "_source": {
+                        "title": "넷플릭스, ‘지금 우리 학교는‘ 시즌2 제작 확정"
+                    }
+                },
+                {
+                    "_id": "sisajournal-240122",
+                    "_source": {
+                        "title": "北 미사일 바라보는 日 정부와 시민들 ‘온도차 뚜렷’"
+                    }
+                },
+                {
+                    "_id": "asiae-2022053008591858739",
+                    "_source": {
+                        "title": "디저트로 떠나는 제주 여행"
+                    }
+                },
+                {
+                    "_id": "mk-202206497151",
+                    "_source": {
+                        "title": "넷플릭스 ‘지금 우리 학교는‘ 후속편 제작 확정"
+                    }
+                },
+                {
+                    "_id": "newsis-NISX20220607_0001899171",
+                    "_source": {
+                        "title": "울릉군, 농수특산물 온라인 쇼핑몰 ‘울릉몰’ 오픈"
+                    }
+                },
+                {
+                    "_id": "fnnews-society-202206070834149204",
+                    "_source": {
+                        "title": "경기도, 주꾸미 10만 마리 화성·안산·시흥 연안 방류"
+                    }
+                },
+                {
+                    "_id": "newsis-NISX20220607_0001898424",
+                    "_source": {
+                        "title": "경기도, 어린 주꾸미 10만마리 화성·안산·시흥 연안 방류"
+                    }
+                },
+                {
+                    "_id": "edaily-01836806632359424",
+                    "_source": {
+                        "title": "‘4차산업 시대’ IP 교양서 ‘스타트업 특허 바이블’"
+                    }
+                },
+                {
+                    "_id": "joongang-25077420",
+                    "_source": {
+                        "title": "칸 접수한 K콘텐트, 그 뒤엔 ‘할리우드형 스튜디오’"
+                    }
+                },
+                {
+                    "_id": "newsis-NISX20220609_0001901040",
+                    "_source": {
+                        "title": "[집중 인터뷰]송강호라는 공기, 송강호라는 뒷배"
+                    }
+                },
+                {
+                    "_id": "joongang-25077099",
+                    "_source": {
+                        "title": "‘친환경’ 입고, 레드카펫 오른 그녀들"
+                    }
+                },
+                {
+                    "_id": "chosun-7RO5MRX53JGYXAQCJXU6WUMP5M",
+                    "_source": {
+                        "title": "[신문은 선생님] [재미있는 과학] 싫은 사람 보면 물 내뿜는 문어… 사투리 쓰는 범고래"
+                    }
+                },
+                {
+                    "_id": "joongang-25076467",
+                    "_source": {
+                        "title": "오징어·바지락·새우 듬뿍, 프라이팬밥…와인 안주로 훌륭해요 [쿠킹]"
+                    }
+                },
+                {
+                    "_id": "sisajournal-239236",
+                    "_source": {
+                        "title": "위기에 빠진 토종 OTT, 해외서 탈출구 찾는다"
+                    }
+                },
+                {
+                    "_id": "joongang-25077329",
+                    "_source": {
+                        "title": "‘브로커‘도 ‘헌트‘도 할리우드 시스템…“K콘텐트 세계화 뒷받침“"
+                    }
+                },
+                {
+                    "_id": "inews24-1487419",
+                    "_source": {
+                        "title": "73개 가공식품 중 4개 빼고 다 올랐다…물가 10년만에 최고"
+                    }
+                },
+                {
+                    "_id": "newsis-NISX20220605_0001897123",
+                    "_source": {
+                        "title": "김해국제의생명과학축제 17~18일 개최…미래먹거리산업 육성"
+                    }
+                },
+                {
+                    "_id": "joongang-25076935",
+                    "_source": {
+                        "title": "그녀가 입은 드레스의 마법…칸 레드카펫이 ‘그린카펫‘ 됐다"
+                    }
+                },
+                {
+                    "_id": "segye-20220606512368",
+                    "_source": {
+                        "title": "“동물도 ‘법적 권리’ 누리는 세상 꿈꿔요” [차 한잔 나누며]"
+                    }
+                },
+                {
+                    "_id": "edaily-01295606632359096",
+                    "_source": {
+                        "title": "국수·식용유값 고공행진…73개 가공식품 중 69개품목 올라"
+                    }
+                },
+                {
+                    "_id": "moneys-2022060417382515705",
+                    "_source": {
+                        "title": "반크 “판소리, 중국문화 아닌 한국의 전통소리“ 시정 캠페인"
+                    }
+                },
+                {
+                    "_id": "sisajournal-239328",
+                    "_source": {
+                        "title": "파라마운트+ 가세한 OTT 대전, 승자는 누가 될까"
+                    }
+                },
+                {
+                    "_id": "sedaily-2674M2MCNO",
+                    "_source": {
+                        "title": "고급 양복부터 연예인 인맥·슈퍼카까지…‘청년재력가‘ 사기꾼들의 특징"
+                    }
+                },
+                {
+                    "_id": "munhwa-2022060501039910025001",
+                    "_source": {
+                        "title": "“보전금 받아도…가게 어려워”…6월도 소상공인 ‘흐림’"
+                    }
+                },
+                {
+                    "_id": "mk-202206494924",
+                    "_source": {
+                        "title": "“파리만 날려요“…지갑 닫은 손님에 수산시장 상인들 ‘한숨‘[르포]"
+                    }
+                },
+                {
+                    "_id": "mk-202206493240",
+                    "_source": {
+                        "title": "“하다하다 ‘판소리‘까지 ‘중국 유산‘?“…황당 주장에 반크 시정 캠페인"
+                    }
+                },
+                {
+                    "_id": "joongang-25076951",
+                    "_source": {
+                        "title": "국수 33%↑·식용유 23%↑…가공식품 7.6%↑, 10년만에 최고 상승"
+                    }
+                },
+                {
+                    "_id": "economist-20220606140007109",
+                    "_source": {
+                        "title": "“4개 빼고 다 올랐다”…국수·식용유 등 가공식품 10년 만에 최고 상승"
+                    }
+                },
+                {
+                    "_id": "heraldcorp-20220604000051",
+                    "_source": {
+                        "title": "해수부, 다음달 3일까지 총 2300톤 비축수산물 푼다…물가 안정 총력"
+                    }
+                },
+                {
+                    "_id": "joongang-25076741",
+                    "_source": {
+                        "title": "中백과사전 버젓이 “판소리는 조선족 유산“…반크 “이게 뭔 소리“"
+                    }
+                },
+                {
+                    "_id": "segye-20220606514498",
+                    "_source": {
+                        "title": "가공식품 물가 10년 만에 최고…저소득층 ‘밥상 물가‘ 어쩌나 [한강로 경제브리핑]"
+                    }
+                },
+                {
+                    "_id": "asiae-2022060614383868936",
+                    "_source": {
+                        "title": "‘서민 식품‘ 국수·밀가루까지…가공식품 가격도 10년 만에 최고 상승"
+                    }
+                },
+                {
+                    "_id": "heraldcorp-20220606000004",
+                    "_source": {
+                        "title": "국수 33%↑·밀가루 26%↑…67개 가공식품 가격 올랐다"
+                    }
+                },
+                {
+                    "_id": "heraldcorp-20220603000654",
+                    "_source": {
+                        "title": "“마동석 주먹이 낫겠다” 볼 게 없는 넷플릭스, 큰일 나겠네"
+                    }
+                }
+            ]
+        }
+    }
+}
+
+from itertools import combinations
+from sklearn.feature_extraction.text import CountVectorizer
+
+from tunip.nugget_api import Nugget, NuggetFilterResultFormat
+
+from tweak.clustering import NncRequest
+from tweak.clustering.distance.bigram_jaccard_dist import (
+    BigramJaccardDistanceCalc,
+    JaccardDistanceCalcRequest,
+    JaccardDistanceCalcResponse,
+)
+from tweak.clustering.factory import NncFactory
+from tweak.utils.text_normalize import normalize_digit
+
+
+max_row = 70
+linkage_method = 'average'
+# dist_threshold = 0.777
+# dist_threshold = 0.822
+# dist_threshold = 0.85
+dist_threshold = 0.92
+use_bigrams_also = True
+
+white_ptags = {
+    'unigram': ['V', 'N', 'SL', 'SH', 'SN'],
+    # 'unigram': ['V', 'N', 'J', 'M', 'SL', 'SH', 'SN'],
+    # 'bigram': ['V', 'N', 'J', 'M', 'SL', 'SH', 'SN'],
+    'bigram': ['V', 'N', 'M', 'SL', 'SH', 'SN'],
+}
+
+
+nugget = Nugget()
+distance_calc = BigramJaccardDistanceCalc()
+
+
+titles = [e['_source']['title'] for e in es_result["es_result"]['hits']['hits']]
+titles = [normalize_digit(t) for t in titles]
+es_doc_ids = [e['_id'] for e in es_result["es_result"]['hits']['hits']]
+# nuggets = list(nugget(titles))
+# title_nuggets = [[t[3] for t in e['tokens']] for e in nuggets]
+
+# print(title_nuggets[0])
+# print(title_nuggets[-1])
+
+corpus_bigrams = []
+corpus_unigrams = []
+bigrams, unigrams = nugget.bigrams_also_selective_tags(texts=titles, white_tags_dict=white_ptags)
+# bigrams, unigrams = nugget.bigrams_also(texts=titles, white_tags=white_ptags, result_format=NuggetFilterResultFormat.NUGGET_B_E_LEX)
+
+for sent in bigrams:
+    corpus_bigrams.append(' '.join(map('_'.join, sent)))
+
+for sent in unigrams:
+    corpus_unigrams.append(' '.join(sent))
+
+corpus = []
+for us, bs in zip(corpus_unigrams, corpus_bigrams):
+    if use_bigrams_also:
+        corpus.append(us + ' ' + bs)
+    else:
+        corpus.append(bs)
+
+vectorizer = CountVectorizer()
+
+xx = vectorizer.fit_transform(corpus)
+# numpy.ndarray
+term_vector = xx.toarray()
+
+print(len(term_vector))
+
+
+dist_calc_req = JaccardDistanceCalcRequest(term_vector= term_vector, num_rows=max_row) # TODO set config from deploy
+dist_calc_res: JaccardDistanceCalcResponse = distance_calc(dist_calc_req)
+
+comb_indices = list(combinations(range(0, max_row), 2))
+print(len(dist_calc_res.distances))
+
+pidx2dist = dict(list(zip(comb_indices, dist_calc_res.distances)))
+    
+print(dist_calc_res.distances[0])
+
+hac = NncFactory.create("HAC", dist_threshold)
+nnc_req = NncRequest(
+    distances=dist_calc_res.distances,
+    dist_calc_status=dist_calc_res.status,
+    method=linkage_method
+)
+# docid_clusters = hac(nnc_req)
+docid_clusters, dist_clusters = hac.detail(nnc_req)
+
+print(docid_clusters)
+print(dist_clusters)
+
+# print('\n'.join([corpus[28], corpus[30], corpus[40]]))
+
+num_clus = []
+for cids, dists in zip(docid_clusters, dist_clusters):
+    print(' | '.join(list(map(lambda d: str(d), dists))))
+    print('\n'.join(list(map(lambda c: corpus[c], cids))))
+    print('================================')
+    num_clus.append(len(cids))
+
+avg_items_clu = sum(num_clus) / len(docid_clusters)
+
+print(f"linkage method: {linkage_method}")
+print(f"pair distance threshold for clustering: {dist_threshold}")
+print(f"use bigrams and unigrams: {use_bigrams_also}")
+print(f"{len(docid_clusters)} number of clusters")
+print(f"{avg_items_clu} average number of items for each cluster")

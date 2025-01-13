765e14f28ce0334f2eefe4ca61a28eb40614e6fc
ed <cloudjo21@gmail.com>
2025-01-13 09:47:33 +0900

    chore: version 0.2.5

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except ImportError:
 
 setup(
     name="tweak",
-    version=sic("0.2.3"),
+    version=sic("0.2.4"),
     url="https://github.com/cloudjo21/tweak.git",
     packages=find_packages("src"),
     package_dir={"tweak": "src/tweak"},
